fa8a5c2cbedf614d2b64b468eec0a6e5308770de
Wesley Jonathan Marcolino <w.jonathan.marcolino@accenture.com>
2020-03-24 09:34:31 -0300

    Add other test scenarios for PasswordValidator

--- app/src/test/java/br/com/bootcampmarco2020/PasswordValidatorTest.kt
+++ app/src/test/java/br/com/bootcampmarco2020/PasswordValidatorTest.kt
@@ -1,14 +1,51 @@
 package br.com.bootcampmarco2020
 
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class PasswordValidatorTest {
+    private val passwordValidator = PasswordValidator()
 
     @Test
     fun givenPasswordIsShorterThan8_whenValidate_shouldReturnFalse() {
-        val result = PasswordValidator().isValid("12345678")
+        val result = passwordValidator.isValid("aBd32!")
 
         assertFalse(result)
+    }
+
+    @Test
+    fun givenPasswordDoesntContainUppercaseLetter_whenValidate_shouldReturnFalse() {
+        val result = passwordValidator.isValid("abcd123!!@")
+
+        assertFalse(result)
+    }
+
+    @Test
+    fun givenPasswordDoesntContainLowercaseLetter_whenValidate_shouldReturnFalse() {
+        val result = passwordValidator.isValid("ABCD123!!@")
+
+        assertFalse(result)
+    }
+
+    @Test
+    fun givenPasswordDoesntContainANumber_whenValidate_shouldReturnFalse() {
+        val result = passwordValidator.isValid("ABCDcfdf!!@")
+
+        assertFalse(result)
+    }
+
+    @Test
+    fun givenPasswordDoesntContainASpecialCharacter_whenValidate_shouldReturnFalse() {
+        val result = passwordValidator.isValid("ABCDcfdf323")
+
+        assertFalse(result)
+    }
+
+    @Test
+    fun givenPasswordIsValid_whenValidate_shouldReturnTrue() {
+        val result = passwordValidator.isValid("abcdDEas@!@!2133")
+
+        assertTrue(result)
     }
 }
